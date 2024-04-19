2be11a31f2e4369fe660a6f2ac3b4a743d8d61fc
clavedeluna <danalitovsky+git@gmail.com>
2024-04-19 09:37:34 -0300

    add sonar knex example

--- routes/tmp.ts
+++ routes/tmp.ts
@@ -1,36 +1,9 @@
-import sqlite3 from 'sqlite3';
+async function index(req, res) {
+    const knex = req.app.get('knex');
 
-// Open the database
-const db = new sqlite3.Database('./mydatabase.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        console.error('Error opening database: ' + err.message);
-    } else {
-        console.log('Database connected!');
-    }
-});
+    let loggedInUser = await knex('users')
+        .whereRaw(`user = '${req.query.user}' and pass = '${req.query.pass}'`); // Noncompliant
 
-// Function that is vulnerable to SQL Injection
-function findUserByUsername(username: string): void {
-    // Unsafe query: directly concatenates the input into the SQL string
-    const query = `SELECT * FROM users WHERE username = '${username}'`;
-
-    db.all(query, [], (error, rows) => {
-        if (error) {
-            console.error('Query error: ' + error.message);
-        } else {
-            console.log('User found:', rows);
-        }
-    });
+    res.send(JSON.stringify(loggedInUser));
+    res.end();
 }
-
-
-findUserByUsername("admin");
-
-// Always close the database connection
-db.close((err) => {
-    if (err) {
-        console.error('Error closing database: ' + err.message);
-    } else {
-        console.log('Database connection closed.');
-    }
-});
